e1e4ce7434dcac06e210c5c0f4c2e2df13bfc4dd
florent524 <florent.guillaume@student.uclouvain.be>
2025-03-14 15:45:11 +0100

    Update PV_dynamic.py

--- PV_dynamic.py
+++ PV_dynamic.py
@@ -1,4 +1,4 @@
-from pyomo.environ import ConcreteModel, Var, Objective, Constraint, NonNegativeReals, minimize, SolverFactory
+from pyomo.environ import ConcreteModel, Var, Objective, Constraint, NonNegativeReals, minimize, SolverFactory, value
 import matplotlib.pyplot as plt
 
 # Data / Parameters
@@ -15,67 +15,64 @@ chargetime = 4  # hours to charge fully the battery
 model = ConcreteModel()
 
 # Define model variables
-model.pv = Var( domain=NonNegativeReals)
+model.pv = Var(domain=NonNegativeReals)
 model.E_pv = Var(range(timestep), domain=NonNegativeReals)
-model.batt_max = Var( domain=NonNegativeReals)
+model.batt_max = Var(domain=NonNegativeReals)
 model.batt = Var(range(timestep), domain=NonNegativeReals)
 model.batt_in = Var(range(timestep), domain=NonNegativeReals)
 model.batt_out = Var(range(timestep), domain=NonNegativeReals)
 
 # Define the constraints
-
-
 def power_balance(model, t):
-    return model.E_pv[t] + model.batt_in[t]- model.batt_out[t] == load[t] 
+    return model.E_pv[t] + model.batt_out[t] - model.batt_in[t] == load[t]
 model.power_balance = Constraint(range(timestep), rule=power_balance)
 
 def pv_production(model, t):
-    return model.E_pv[t] == model.pv*lf_pv[t]
+    return model.E_pv[t] == model.pv * lf_pv[t]
 model.pv_production = Constraint(range(timestep), rule=pv_production)
 
-def battery_charge_rate (model, t):
-    if model.E_pv[t] - load[t] < 0:
-        model.batt_in[t] = model.E_pv[t] - load[t]
-    else:
-        model.batt_in[t] = 0
+def battery_charge_rate(model, t):
     return model.batt_in[t] <= model.batt_max / chargetime
 model.battery_charge_rate = Constraint(range(timestep), rule=battery_charge_rate)
 
-def battery_discharge_rate (model, t):
-    if load[t] - model.E_pv[t] > 0:
-        model.batt_out[t] = load[t] - model.E_pv[t]
-    else:
-        model.batt_out[t] = 0
+def battery_discharge_rate(model, t):
     return model.batt_out[t] <= model.batt_max / chargetime
 model.battery_discharge_rate = Constraint(range(timestep), rule=battery_discharge_rate)
 
 def battery_balance(model, t):
     if t == 0:
-        return model.batt[t] == model.batt_in[t]*eff_batt_in - model.batt_out[t]/eff_batt_out
+        return model.batt[t] == model.batt_max * 0.5
     else:
-        return model.batt[t] == model.batt[t-1] + model.batt_in[t]*eff_batt_in - model.batt_out[t]/eff_batt_out
+        return model.batt[t] == model.batt[t - 1] + model.batt_in[t] * eff_batt_in - model.batt_out[t] / eff_batt_out
 model.battery_balance = Constraint(range(timestep), rule=battery_balance)
 
-
-
-# Define the objective functions
+# Define the objective function
 def obj_rule(model):
-    return model.pv*c_pv + model.batt_max*c_batt
+    return model.pv * c_pv + model.batt_max * c_batt
 model.obj = Objective(rule=obj_rule, sense=minimize)
 
-
-# Specify the path towards your solver (gurobi) file
+# Solve the model
 solver = SolverFactory('gurobi')
 solver.solve(model)
 
 # Results - Print the optimal PV size and optimal battery capacity
-##########################################
-############ CODE TO ADD HERE ############
-##########################################
+print(f"Optimal PV size: {value(model.pv)} kW")
+print(f"Optimal Battery Capacity: {value(model.batt_max)} kWh")
 
+# Extract results for plotting
+load_vals = load
+pv_vals = [value(model.E_pv[t]) for t in range(timestep)]
+batt_vals = [value(model.batt[t]) for t in range(timestep)]
 
-# Plotting - Generate a graph showing the evolution of (i) the load, 
-# (ii) the PV production and, (iii) the soc of the battery
-##########################################
-############ CODE TO ADD HERE ############
-##########################################
+# Plot results
+plt.figure(figsize=(10, 5))
+plt.plot(load_vals, label="Load (kW)", linestyle="--", marker="o")
+plt.plot(pv_vals, label="PV Production (kW)", linestyle="-", marker="s")
+plt.plot(batt_vals, label="Battery SOC (kWh)", linestyle="-", marker="d")
+plt.xlabel("Time (hours)")
+plt.ylabel("Power/Energy (kW/kWh)")
+plt.title("Energy System Behavior")
+plt.legend()
+plt.grid()
+plt.show()
+# End of PV_dynamic.py

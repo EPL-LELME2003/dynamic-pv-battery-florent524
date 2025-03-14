ef645610670e31cf06a019f9ed1827e7e112ff2d
florent524 <florent.guillaume@student.uclouvain.be>
2025-03-14 15:34:55 +0100

    Update PV_dynamic.py

--- PV_dynamic.py
+++ PV_dynamic.py
@@ -15,23 +15,57 @@ chargetime = 4  # hours to charge fully the battery
 model = ConcreteModel()
 
 # Define model variables
-##########################################
-############ CODE TO ADD HERE ############
-##########################################
+model.pv = Var( domain=NonNegativeReals)
+model.E_pv = Var(range(timestep), domain=NonNegativeReals)
+model.batt_max = Var( domain=NonNegativeReals)
+model.batt = Var(range(timestep), domain=NonNegativeReals)
+model.batt_in = Var(range(timestep), domain=NonNegativeReals)
+model.batt_out = Var(range(timestep), domain=NonNegativeReals)
 
 # Define the constraints
-##########################################
-############ CODE TO ADD HERE ############
-##########################################
+
+
+def power_balance(model, t):
+    return model.E_pv[t] + model.batt_in[t]- model.batt_out[t] == load[t] 
+model.power_balance = Constraint(range(timestep), rule=power_balance)
+
+def pv_production(model, t):
+    return model.E_pv[t] == model.pv*lf_pv[t]
+model.pv_production = Constraint(range(timestep), rule=pv_production)
+
+def battery_charge_rate (model, t):
+    if model.E_pv[t] - load[t] < 0:
+        model.batt_in[t] = model.E_pv[t] - load[t]
+    else:
+        model.batt_in[t] = 0
+    return model.batt_in[t] <= model.batt_max / chargetime
+model.battery_charge_rate = Constraint(range(timestep), rule=battery_charge_rate)
+
+def battery_discharge_rate (model, t):
+    if load[t] - model.E_pv[t] > 0:
+        model.batt_out[t] = load[t] - model.E_pv[t]
+    else:
+        model.batt_out[t] = 0
+    return model.batt_out[t] <= model.batt_max / chargetime
+model.battery_discharge_rate = Constraint(range(timestep), rule=battery_discharge_rate)
+
+def battery_balance(model, t):
+    if t == 0:
+        return model.batt[t] == model.batt_in[t]*eff_batt_in - model.batt_out[t]/eff_batt_out
+    else:
+        return model.batt[t] == model.batt[t-1] + model.batt_in[t]*eff_batt_in - model.batt_out[t]/eff_batt_out
+model.battery_balance = Constraint(range(timestep), rule=battery_balance)
+
+
 
 # Define the objective functions
-##########################################
-############ CODE TO ADD HERE ############
-##########################################
+def obj_rule(model):
+    return model.pv*c_pv + model.batt_max*c_batt
+model.obj = Objective(rule=obj_rule, sense=minimize)
 
 
 # Specify the path towards your solver (gurobi) file
-solver = SolverFactory('...')
+solver = SolverFactory('gurobi')
 solver.solve(model)
 
 # Results - Print the optimal PV size and optimal battery capacity
